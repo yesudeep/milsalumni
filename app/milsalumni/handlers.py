34d105d6cdabe352cccd293f14afa1ba98282b20
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-11-01 04:11:44 +0530

    Get the ChromeFrameMiddleware middleware to work.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@gmail.com>

--- app/milsalumni/handlers.py
+++ app/milsalumni/handlers.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-    hello_world.handlers
-    ~~~~~~~~~~~~~~~~~~~~
 
-    Hello, World!: the simplest tipfy app.
-
-    :copyright: 2009 by tipfy.org.
-    :license: BSD, see LICENSE for more details.
-"""
 from tipfy import RequestHandler, Response
 from tipfyext.jinja2 import Jinja2Mixin
 
+from middleware import ChromeFrameMiddleware
 
-class HelloWorldHandler(RequestHandler):
+class PageRequestHandler(RequestHandler, Jinja2Mixin):	
+	middleware = [ChromeFrameMiddleware()]
+
+class IndexHandler(PageRequestHandler):
     def get(self):
-        """Simply returns a Response object with an enigmatic salutation."""
-        return Response('Hello, World!')
-
-
-class PrettyHelloWorldHandler(RequestHandler, Jinja2Mixin):
-    def get(self):
-        """Simply returns a rendered template with an enigmatic salutation."""
-        context = {
-            'message': 'Hello, World!',
-        }
-        return self.render_response('hello_world.html', **context)
+        return self.render_response('index.html', message="Hello world")
--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,5 @@
 from tipfy import Rule
 
 rules = [
-    Rule('/', name='hello-world', handler='hello_world.handlers.HelloWorldHandler'),
-    Rule('/pretty', name='hello-world-pretty', handler='hello_world.handlers.PrettyHelloWorldHandler'),
+    Rule('/', name='index', handler='milsalumni.handlers.IndexHandler'),
 ]
