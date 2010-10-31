b36748f3a2fb8137d25ba5684b8c56a177c1d647
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-11-01 04:49:09 +0530

    Add a TemplateContextMiddleware instance to the PageRequestHandler.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@gmail.com>

--- app/milsalumni/handlers.py
+++ app/milsalumni/handlers.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import config
 from tipfy import RequestHandler, Response
 from tipfyext.jinja2 import Jinja2Mixin
 
-from middleware import ChromeFrameMiddleware
+from middleware import ChromeFrameMiddleware, TemplateContextMiddleware
 
-class PageRequestHandler(RequestHandler, Jinja2Mixin):	
-	middleware = [ChromeFrameMiddleware()]
+template_context_middleware = TemplateContextMiddleware(
+	DEBUG=config.DEBUG,
+	APPLICATION_TITLE=config.APPLICATION_TITLE,
+	CSS_URL=config.CSS_URL,
+	IMAGE_URL=config.IMAGE_URL,
+	JS_URL=config.JS_URL,
+	JS_MINIFICATION_SUFFIX=config.JS_MINIFICATION_SUFFIX,
+	CSS_MINIFICATION_SUFFIX=config.CSS_MINIFICATION_SUFFIX,
+	)
+
+class PageRequestHandler(RequestHandler, Jinja2Mixin):
+	middleware = [ChromeFrameMiddleware(), template_context_middleware]
 
 class IndexHandler(PageRequestHandler):
     def get(self):
-        return self.render_response('index.html', message="Hello world")
+        #return self.render_response('index.html', message="Hello world")
+
+		return Response(str(self.context))
+
