46b91cdd78515146f2912502f5c3c2589d87c6e3
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-11-01 04:47:55 +0530

    Update configuration in app/app.yaml and app/config.py.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@gmail.com>

--- app/config.py
+++ app/config.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
 """App configuration."""
+
+# Make this false before deploying on production.
+DEBUG = True
+#DEBUG = False
+
+APPLICATION_TITLE = 'MILS Alumni'
+CSS_URL = '/css/'
+JS_URL = '/js/'
+IMAGE_URL = '/image/'
+
+JS_MINIFICATION_SUFFIX = '.min.js'
+CSS_MINIFICATION_SUFFIX = '.min.css'
+
 config = {}
 
+config['tipfy'] = {
+	'auth_store_class': 'tipfy.auth.MultiAuthStore',
+	'enable_debugger': DEBUG,
+}
