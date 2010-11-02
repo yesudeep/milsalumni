ffdb5e438b8632ae306c4ac46618c24a9aef76e0
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-11-02 22:24:18 +0530

    Add ETagMiddleware to the PageRequestHandler in milsalumni/handlers.py
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@gmail.com>

--- app/milsalumni/handlers.py
+++ app/milsalumni/handlers.py
@@ -4,14 +4,16 @@ import config
 from config import meta_config
 from tipfy import RequestHandler, Response
 from tipfyext.jinja2 import Jinja2Mixin
+from tipfy.middleware import ETagMiddleware
 from middleware import ChromeFrameMiddleware, TemplateContextMiddleware
+
 
 template_context = dict(DEBUG=config.DEBUG)
 template_context.update(meta_config)
 template_context_middleware = TemplateContextMiddleware(**template_context)
 
 class PageRequestHandler(RequestHandler, Jinja2Mixin):
-	middleware = [ChromeFrameMiddleware(), template_context_middleware]
+	middleware = [ETagMiddleware(), ChromeFrameMiddleware(), template_context_middleware]
 
 class IndexHandler(PageRequestHandler):
     def get(self):
