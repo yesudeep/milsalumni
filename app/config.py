bd2c2a23bc76a50ca08b341148903a162b6b588c
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-11-02 22:17:20 +0530

    Include session secret into configuration and fix missing APPLICATION_TITLE in base.html
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@gmail.com>

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """App configuration."""
 
+from secrets import secret_key
 from tipfy.app import APPENGINE, DEV_APPSERVER
 
 # Make this false before deploying on production.
@@ -18,7 +19,7 @@ else:
 	ANALYTICS_CODE = """<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(['_setAccount', '%(GOOGLE_ANALYTICS_ID)s']);_gaq.push(['_trackPageview']);(function(){var doc=document,ga=doc.createElement('script');ga.src=('https:'==doc.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';ga.setAttribute('async', 'true');doc.documentElement.firstChild.appendChild(ga);})();</script>""" % dict(GOOGLE_ANALYTICS_ID=GOOGLE_ANALYTICS_ID)
 
 meta_config = dict(
-	APPLICATION_TITLE = 'MILS Alumni',
+	APPLICATION_TITLE='MILS Alumni',
 	CSS_URL='/css/',
 	JS_URL=JS_URL,
 	IMAGE_URL='/image/',
@@ -33,4 +34,7 @@ config = {}
 config['tipfy'] = {
 	'auth_store_class': 'tipfy.auth.MultiAuthStore',
 	'enable_debugger': DEBUG,
-}
+        }
+config['tipfy.sessions'] = {
+        'secret_key': secret_key,
+        }
