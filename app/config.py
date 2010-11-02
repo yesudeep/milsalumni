71d21738d50bad3b60cd1983a1fe734fd3c4ddad
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-11-02 16:37:39 +0530

    Set up basic meta configuration and import them into the base template.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@gmail.com>

--- app/config.py
+++ app/config.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
 """App configuration."""
 
+from tipfy.app import APPENGINE, DEV_APPSERVER
+
 # Make this false before deploying on production.
 DEBUG = True
 #DEBUG = False
 
-APPLICATION_TITLE = 'MILS Alumni'
-CSS_URL = '/css/'
-JS_URL = '/js/'
-IMAGE_URL = '/image/'
+GOOGLE_ANALYTICS_ID = 'UA-7340598-3'
 
-JS_MINIFICATION_SUFFIX = '.min.js'
-CSS_MINIFICATION_SUFFIX = '.min.css'
+JS_URL = '/js/'
+if DEV_APPSERVER:
+	JQUERY_URL = JS_URL + 'vendor/jquery-1.4.3.min.js'
+	ANALYTICS_CODE = ''
+else:
+	JQUERY_URL = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js'
+	ANALYTICS_CODE = """<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(['_setAccount', '%(GOOGLE_ANALYTICS_ID)s']);_gaq.push(['_trackPageview']);(function(){var doc=document,ga=doc.createElement('script');ga.src=('https:'==doc.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';ga.setAttribute('async', 'true');doc.documentElement.firstChild.appendChild(ga);})();</script>""" % dict(GOOGLE_ANALYTICS_ID=GOOGLE_ANALYTICS_ID)
+
+meta_config = dict(
+	APPLICATION_TITLE = 'MILS Alumni',
+	CSS_URL='/css/',
+	JS_URL=JS_URL,
+	IMAGE_URL='/image/',
+	JS_MINIFICATION_SUFFIX='.min.js',
+	CSS_MINIFICATION_SUFFIX='.min.css',
+	JQUERY_URL=JQUERY_URL,
+	ANALYTICS_CODE=ANALYTICS_CODE,
+)
 
 config = {}
 
--- app/milsalumni/handlers.py
+++ app/milsalumni/handlers.py
@@ -1,27 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import config
+from config import meta_config
 from tipfy import RequestHandler, Response
 from tipfyext.jinja2 import Jinja2Mixin
-
 from middleware import ChromeFrameMiddleware, TemplateContextMiddleware
 
-template_context_middleware = TemplateContextMiddleware(
-	DEBUG=config.DEBUG,
-	APPLICATION_TITLE=config.APPLICATION_TITLE,
-	CSS_URL=config.CSS_URL,
-	IMAGE_URL=config.IMAGE_URL,
-	JS_URL=config.JS_URL,
-	JS_MINIFICATION_SUFFIX=config.JS_MINIFICATION_SUFFIX,
-	CSS_MINIFICATION_SUFFIX=config.CSS_MINIFICATION_SUFFIX,
-	)
+template_context = dict(DEBUG=config.DEBUG)
+template_context.update(meta_config)
+template_context_middleware = TemplateContextMiddleware(**template_context)
 
 class PageRequestHandler(RequestHandler, Jinja2Mixin):
 	middleware = [ChromeFrameMiddleware(), template_context_middleware]
 
 class IndexHandler(PageRequestHandler):
     def get(self):
-        #return self.render_response('index.html', message="Hello world")
-
-		return Response(str(self.context))
-
+        return self.render_response('index.html', **self.context)
